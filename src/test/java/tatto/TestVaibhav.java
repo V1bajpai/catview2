ad430e8271985748d13c6c35c1c704d5f8781f45
Vaibhav bajpai <vaibhavbajpai07059@gmail.com>
2021-10-04 18:15:22 +0530

    Post request in RestAssured

--- src/test/java/tatto/TestVaibhav.java
+++ src/test/java/tatto/TestVaibhav.java
@@ -1,45 +1,43 @@
 package tatto;
 
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matcher.*;
-import org.json.JSONArray;
-import org.json.JSONException;
+
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import io.restassured.RestAssured;
-//import io.restassured.RestAssured.*;
-import io.restassured.http.ContentType;
-import io.restassured.matcher.RestAssuredMatchers.*;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
+
 import java.util.*;
 
 public class TestVaibhav {
 		@Test
 		public void verifyJSONArrayResponse() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vaibhav\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\browserdriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
+		
+		//1. Get Request operations
 		JsonArray jsonArray = new JsonArray();
 		jsonArray = given().baseUri("https://api.thecatapi.com")
 				.basePath("/v1/images/search")
 				.get().as(JsonArray.class);
 
+		//2. Read data and producing o/p on browser
 		for (int i = 0; i < jsonArray.size(); i++) {
 			JsonObject jsonObject = jsonArray.get(i).getAsJsonObject();
 			String urlcat=jsonObject.get("url").getAsString();
 			System.out.println(urlcat);
 			driver.manage().window().maximize();
 			driver.get(urlcat);
-
 		}
 		System.out.println("Do you want to another search: ");
 		System.out.println("Press-1 for Continue");
@@ -50,4 +48,27 @@ public class TestVaibhav {
 			verifyJSONArrayResponse();
 		}		
 	}
+		
+		
+		// Post Request for voting
+		@Test
+		public void RegistrationSuccessful()
+		{		
+			RestAssured.baseURI ="https://api.thecatapi.com";
+			String payload = "{\r\n"
+					+ "  \"image_id\": \"asf2\",\r\n"
+					+ "  \"sub_id\": \"my-user-1234\",\r\n"
+					+ "  \"value\": 1\r\n"
+					+ "}";
+			RequestSpecification request = RestAssured.given();
+			request.header("Content-type", "application/json");
+			
+			Response response = request.body(payload).post("/v1/votes");
+			
+			int statusCode = response.getStatusCode();
+			System.out.println("Response status code is: "+ statusCode);
+			Assert.assertEquals(200, statusCode);
+		}
+		
+
 }
